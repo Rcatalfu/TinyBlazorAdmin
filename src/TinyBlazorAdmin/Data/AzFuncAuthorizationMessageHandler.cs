44a87e0d8ea8e7750836c5db427aedc1a4737a52
Ryan Catalfu <46333049+Rcatalfu@users.noreply.github.com>
2021-04-03 15:29:32 -0400

    Update AzFuncAuthorizationMessageHandler.cs

--- src/TinyBlazorAdmin/Data/AzFuncAuthorizationMessageHandler.cs
+++ src/TinyBlazorAdmin/Data/AzFuncAuthorizationMessageHandler.cs
@@ -25,10 +25,9 @@ namespace TinyBlazorAdmin.Data
         {
             var section = config.GetSection(nameof(UrlShortenerSecuredService));
             Endpoint = section.GetValue<string>(nameof(Endpoint));
-            console.log(Endpoint);
             ConfigureHandler(
                 new[] { Endpoint },
-                new[] { string.Concat(Endpoint, "/user_impersonation") }
+                new[] { string.Concat(Endpoint, "user_impersonation") }
             );
         }
     }
